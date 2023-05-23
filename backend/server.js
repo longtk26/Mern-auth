5aee71ad116b8435649202649e8f7b177ce953e4
longtk26 <longtk26@gmail.com>
2023-05-23 22:44:27 +0700

    Set up server, userRoutes, handle error and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 5000;
+
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("API running...");
-})
+});
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`);
-})
+app.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`);
+});
