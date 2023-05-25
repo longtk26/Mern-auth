1e68faf014e84567a781b541b165ab967441d41d
longtk26 <longtk26@gmail.com>
2023-05-25 10:33:36 +0700

    auth, logout, get-profile and update-profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,24 @@ import generateToken from "../utils/generateToken.js";
 // @route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Success" });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    const isCorrectPassword = await user.matchPassword(password);
+
+    if (user && isCorrectPassword) {
+        generateToken(res, user._id);
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 // @desc    Register a new user
@@ -46,21 +63,55 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Logging you out..." });
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: "Logged out successfully!" });
 });
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.json({ message: "Getting user profile..." });
+    const user = req.user;
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send("update profile");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 export {
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -13,6 +14,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
