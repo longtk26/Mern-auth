2f3f6083154a5c590ed805f09553c8e5b66a3bf8
longtk26 <longtk26@gmail.com>
2023-05-25 08:57:44 +0700

    user-register-controller, protect-middleware, hashing, generate-token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,8 @@
 import asyncHandler from "express-async-handler";
 
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+
 // @desc Auth user & get token
 // @route POST /api/users/auth
 // @access Public
@@ -11,7 +14,32 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register user success!" });
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("Invalid user data");
+    }
 });
 
 // @desc    Logout user / clear cookie
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
